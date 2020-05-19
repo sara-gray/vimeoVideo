d41411e830ef9388aebeba08bf5b5265105cfe80
Sara Gray <saragrayen@gmail.com>
2020-05-19 12:21:13 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,62 +6,35 @@ const videos = document.getElementById('videos');
 
 let options = {
   id: 59777392,
-  width: 640,
-  loop: true
+  autoplay: true,
+  color: '00adef',
+  loop: true,
 };
 
 let player = new Vimeo.Player('myVideo', options);
 player.setVolume(0);
 
 const playVimeo = function (id) {
-  // unload video
   // load new video
+  player
+    .loadVideo(id)
+    .then(function (id) {
+      console.log(`Vimeo video ${id} successfully loaded`);
+    })
+    .catch(function (error) {
+      console.log(`Vimeo video ${id} player error: ${error.name}`);
+    });
 
-  player.loadVideo(id).then(function (id) {
-    // the video successfully loaded
-    console.log(`Vimeo video ${id} loaded`);
-  }).catch(function (error) {
-    console.log('vimeo error');
-    switch (error.name) {
-      case 'TypeError':
-        // the id was not a number
-        break;
-
-      case 'PasswordError':
-        // the video is password-protected and the viewer needs to enter the
-        // password first
-        break;
-
-      case 'PrivacyError':
-        // the video is password-protected or private
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-  // Start playing
-  player.play().then(function () {
-    // the video was played
-    console.log(`Vimeo video ${id} played`);
-  }).catch(function (error) {
-    switch (error.name) {
-      case 'PasswordError':
-        // the video is password-protected and the viewer needs to enter the
-        // password first
-        break;
-
-      case 'PrivacyError':
-        // the video is private
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-}
+  // Start playing video
+  player
+    .play()
+    .then(function () {
+      console.log(`Vimeo video ${id} is played`);
+    })
+    .catch(function (error) {
+      console.log(`Vimeo video ${id} play error: ${error.name}`);
+    });
+};
 
 videos.addEventListener('click', (e) => {
   let id = e.target.id;
@@ -71,5 +44,4 @@ videos.addEventListener('click', (e) => {
   id = videoID[id - 1];
 
   playVimeo(id);
-
 });
